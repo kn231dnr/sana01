d6594276a273587d1afe1459b57bb557eaf5aaf9
Nazarii Dmytrevych <kn231_dnr@student.ztu.edu.ua>
2024-01-18 19:36:40 +0200

    Створення локального репозиторію

---
+++ sana01/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace LinearExpressions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("пупупу");
+        }
+
+    }
+}
