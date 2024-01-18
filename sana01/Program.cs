404182faa1d186af29e87669a9c93c39ef68806b
Nazarii Dmytrevych <kn231_dnr@student.ztu.edu.ua>
2024-01-18 19:48:32 +0200

    введення всіх значень + українізація

--- sana01/Program.cs
+++ sana01/Program.cs
@@ -1,10 +1,22 @@
-﻿namespace LinearExpressions
+﻿using System.Text;
+
+namespace LinearExpressions
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("пупупу");
+            Console.InputEncoding = Encoding.Unicode;
+            Console.OutputEncoding = Encoding.Unicode;
+            Console.WriteLine("Введіть значення a");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введіть значення b");
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введіть значення c");
+            int c = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введіть значення d");
+            int d = int.Parse(Console.ReadLine());
+
         }
 
     }
