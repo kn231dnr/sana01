41a553d4374738c7d175941d5c394c08b8a56a42
Nazarii Dmytrevych <kn231_dnr@student.ztu.edu.ua>
2024-01-19 15:34:55 +0200

    обрахунок і виведення z1 у проекті 2

--- LinearExpressions2/Program.cs
+++ LinearExpressions2/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Text;
+
+
+namespace LinearExpressions2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.InputEncoding = Encoding.Unicode;
+            Console.OutputEncoding = Encoding.Unicode;
+            Console.WriteLine("Введіть значення a");
+            double a = Double.Parse(Console.ReadLine());
+            Console.WriteLine("Введіть значення b");
+            double b = Double.Parse(Console.ReadLine());
+            Console.WriteLine("Введіть значення m");
+            double m = Double.Parse(Console.ReadLine());
+            Console.WriteLine("Введіть значення n");
+            double n = Double.Parse(Console.ReadLine());
+            Console.WriteLine("Введіть значення x");
+            double x = Double.Parse(Console.ReadLine());
+            double z1 = ((m - 1) * Math.Sqrt(m) - (n - 1) * Math.Sqrt(n)) / (Math.Sqrt(Math.Pow(m, 3) * n) + n * m + Math.Pow(n, 2) - m);
+            Console.WriteLine($"z1 = {z1}");
+        }
+    }
+}
