ed1f841203357fa77d573617d50afffbd5d0709b
Nazarii Dmytrevych <kn231_dnr@student.ztu.edu.ua>
2024-01-19 15:13:09 +0200

    обрахунок та виведення зет

--- sana01/Program.cs
+++ sana01/Program.cs
@@ -22,6 +22,8 @@ namespace LinearExpressions
             Console.WriteLine($"x = {x}");
             double y = (5 * (a + b) * (c - d) / (1.0/2.0 * c)) + (Math.Pow(d, 2) * (Math.Pow(a, 2) - Math.Pow(b, 2)) / (b - a));
             Console.WriteLine($"y = {y}");
+            double z = ((Math.Pow((Math.Pow(x, 2) - 2 * x), 3) - 4 * (Math.Pow(x, 4) + 1)) * (1 - b)) / (5 * a + 3 * b);
+            Console.WriteLine($"z = {z}");
         }
     }
 }
